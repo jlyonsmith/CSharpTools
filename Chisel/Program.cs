1cc241d0573ef641529e9e75e62a3e08ec9cde3c
jlyonsmith <john@lyon-smith.org>
2013-04-21 17:56:04 -0700

    Adding Chisel tool project

---
+++ Chisel/Program.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Chalk.MacOS
+{
+	class MainClass
+	{
+		public static int Main(string[] args)
+		{
+			ChiselTool tool = new ChiselTool();
+
+			try
+			{
+				tool.ProcessCommandLine(args);
+
+				tool.Execute();
+				return (tool.HasOutputErrors ? 1 : 0);
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine("Error: Exception: {0}", exception);
+				return 1;
+			}
+		}
+	}
+}
---
+++ Chisel/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyTitle("Chisel")]
+[assembly: AssemblyDescription("Tab/space fixer")]
+[assembly: AssemblyCompany("John Lyon-Smith")]
+[assembly: AssemblyProduct("Tools")]
+[assembly: AssemblyCopyright("Copyright © John Lyon-Smith 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.20317.4")]
